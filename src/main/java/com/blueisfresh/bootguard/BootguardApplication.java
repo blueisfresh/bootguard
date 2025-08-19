c0c237bbb13de4bc35b1a9a100b01267fc340462
blueisfresh <elias.guerematchi.vrabl@gmail.com>
2025-08-19 14:33:01 +0200

    git init

---
+++ src/main/java/com/blueisfresh/bootguard/BootguardApplication.java
@@ -0,0 +1,13 @@
+package com.blueisfresh.bootguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootguardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootguardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blueisfresh/bootguard/BootguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueisfresh.bootguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootguardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
