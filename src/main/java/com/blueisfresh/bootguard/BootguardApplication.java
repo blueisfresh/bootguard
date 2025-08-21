7d5e3f77b73b49506564fb7d26301d64e3e321a4
blueisfresh <elias.guerematchi.vrabl@gmail.com>
2025-08-21 09:22:24 +0200

    User Updated & Created Time Bug Solved

--- src/main/java/com/blueisfresh/bootguard/BootguardApplication.java
+++ src/main/java/com/blueisfresh/bootguard/BootguardApplication.java
@@ -2,8 +2,10 @@ package com.blueisfresh.bootguard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BootguardApplication {
 
     public static void main(String[] args) {
